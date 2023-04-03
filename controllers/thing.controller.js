883b39433479227e0fdaa5cca8dc38652bcb509b
Ivan Yaremko <yaremich98@gmail.com>
2023-04-03 08:03:24 +0300

    (98) PG HTTP CRUD

---
+++ controllers/thing.controller.js
@@ -0,0 +1,39 @@
+const { Thing } = require('../models');
+
+module.exports.createThing = async (req, res, next) => {
+    try {
+        const {body} = req;
+        const [newThing] = await Thing.create(body);
+        if(newThing){
+            res.status(201).send({data: newThing});
+        }
+        res.status(400).send('not create');
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.getAllThings = async (req, res, next) => {
+    try {
+        const arrayThings = await Thing.findAll();
+        if(arrayThings.length){
+            res.status(200).send({data: newThing});
+        }
+        res.status(400).send();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.getThing = async (req, res, next) => {
+    try {
+        const {params:{id}} = req;
+        const thing = await Thing.findByPk(id);
+        if(thing){
+            res.status(200).send({data: thing});
+        }
+        res.status(400).send();
+    } catch (error) {
+        next(error);
+    }
+}
